a40a904eb20ae9df8a23964a123a73ed98007605
rahulcaet <63642672+rahulcaet@users.noreply.github.com>
2020-05-12 19:00:49 +0530

    Add files via upload

--- getDBnCollectionSize.py
+++ getDBnCollectionSize.py
@@ -2,14 +2,14 @@ def getCollectionSize(dbClient, database, collectionName):
     db = dbClient[database]
 
     collectionStat = db.command("collstats", collectionName)
-    if collectionStat['storageSize'] > (1024*1024):
+    if collectionStat['storageSize'] > (1024*1024*1024):
         unit = 'GB'
-        divisor = 1024*1024
+        divisor = 1024*1024*1024
     else:
         unit = 'MB'
-        divisor = 1024
+        divisor = 1024*1024
 
-    collectionSize = str((collectionStat['storageSize']) / divisor) + unit;
+    collectionSize = str((collectionStat['storageSize']) / divisor) + unit
 
     #print('Size of collection is: {}'.format(collectionSize))
 
@@ -20,12 +20,12 @@ def getDBSize(dbClient, database):
     db = dbClient[database]
 
     dbStat = db.command("dbstats")
-    if dbStat['storageSize'] > (1024*1024):
+    if dbStat['storageSize'] > (1024*1024*1024):
         unit = 'GB'
-        divisor = 1024*1024
+        divisor = 1024*1024*1024
     else:
         unit = 'MB'
-        divisor = 1024
+        divisor = 1024*1024
 
     dbSize = str((dbStat['storageSize']) / divisor) + unit
 
